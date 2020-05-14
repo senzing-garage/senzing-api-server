b2fef81463e8f41f2c298420884c6db5a731e183
Barry M. Caceres <barrycaceres@icloud.com>
2020-05-13 17:52:06 -0700

    Fixed windows build error interpretting File.separator as a regular expression in replaceAll() call

--- src/test/java/com/senzing/nativeapi/replay/ReplayNativeApiProvider.java
+++ src/test/java/com/senzing/nativeapi/replay/ReplayNativeApiProvider.java
@@ -165,7 +165,9 @@ public class ReplayNativeApiProvider implements NativeApiProvider {
    */
   private static final File RESOURCE_CACHE_DIR
       = new File(TEST_RESOURCE_DIR,
-                 PACKAGE_PREFIX.replaceAll("\\.", File.separator)
+                 PACKAGE_PREFIX.replaceAll(
+                     "\\.",
+                     File.separator.equals("\\") ? "\\\\" : File.separator)
                      + "cache");
 
   /**
